d163c591cd9c89c6c4c33b3510052792e6c455c1
farhan ibne saif <84982846+farhanibne@users.noreply.github.com>
2022-03-08 17:20:18 +0600

    Update schema.js

--- schemas/schema.js
+++ schemas/schema.js
@@ -2,6 +2,7 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 
+
 import { userSchema } from './userSchema'
 import { ridesSchema } from './ridesSchema'
 import { tripSchema } from './tripSchema'
